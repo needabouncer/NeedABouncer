6bb661e323e36f6ead9b9802192839f674306e10
KCLSU <45687039+KCLSU@users.noreply.github.com>
2020-06-07 19:09:26 +0200

    fix css module

--- src/components/content/landingHeader/landingHeader.js
+++ src/components/content/landingHeader/landingHeader.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
-import classes from './LandingHeaderStyles.module.css';
+import classes from './landingHeaderStyles.module.css';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 import Img from "gatsby-image"
